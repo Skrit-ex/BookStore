b9cded9db681392df5dda12a80316ca3219ba9dc
Skrit-ex <skritex0@gmail.com>
2024-04-01 15:15:11 +0300

    added

---
+++ src/main/java/by/pack/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package by.pack.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+public class LoginDto {
+
+    @Pattern(regexp = "([A-Za-z]*)", message = "The UserName isn't correct, try again")
+    private String userName;
+    @Pattern(regexp = "(^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$)" , message = "The Email isn't correct, try again")
+    private String email;
+}
