f6f8046f25bf44f14e71c8768759b3075c3c69df
Skrit-ex <skritex0@gmail.com>
2024-04-06 21:20:30 +0300

    trying to make saveBook method

--- src/main/java/by/pack/libraryBook/Test.java
+++ src/main/java/by/pack/libraryBook/Test.java
@@ -3,37 +3,45 @@ package by.pack.libraryBook;
 import by.pack.entity.Book;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         saveBook();
     }
-    public static void saveBook() {
-        File file = new File("G:\\JDK\\BookStore\\src\\main\\java\\by\\pack\\libraryBook\\Bookfile");
 
+    public static void saveBook() throws IOException {
+        File file = new File("G:\\JDK\\BookStore\\src\\main\\java\\by\\pack\\libraryBook\\Bookfile");
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    String[] data = line.split(",");
-                    if (data.length >= 4) {
-                        String nameBook = data[0];
-                        String nameAuthor = data[1];
-                        String lastNameAuthor = data[2];
-                        String genre = data[3];
-                        Book book = new Book(nameBook, nameAuthor, lastNameAuthor, genre);
-                        System.out.println(book);
-                    }
-                }
-            }
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
+            String s = bufferedReader.readLine();
+            System.out.println(s);
+        } finally {
 
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                if (!line.isEmpty()) {
+//                    String[] data = line.split(",");
+//                    if (data.length >= 4) {
+//                        String nameBook = data[0];
+//                        String nameAuthor = data[1];
+//                        String lastNameAuthor = data[2];
+//                        String genre = data[3];
+//                        Book book = new Book(nameBook, nameAuthor, lastNameAuthor, genre);
+//                        System.out.println(book);}
+//                    }
+//                }
+//        }catch (FileNotFoundException e){
+//            e.printStackTrace();
+//
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
 
